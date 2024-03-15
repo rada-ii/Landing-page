6f24fcc27b58f24057bdb2fac22034d74ace0c91
rada-i <neposebanmail@gmail.com>
2024-03-15 15:27:39 +0100

    corrections

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 function imageSlider(anything) {
-  document.querySelector(".mn").src = anything;
+  const mainImage = document.querySelector("#mainImage");
+  mainImage.src = anything;
+  // Set hover Strawberry
+  if (anything === "images/RIOTEnergyStrawberrylemonade_550x.png") {
+    mainImage.onmouseover = function () {
+      this.src = "images/RIOTwebfruitcrusherw_typeRollover_550x.png";
+    };
+    mainImage.onmouseout = function () {
+      this.src = "images/RIOTEnergyStrawberrylemonade_550x.png";
+    };
+  }
+  // Hover efekat za Citrus Lime
+  else if (anything === "images/RIOT-Web-Cans-CitrusLime-HP_550x.png") {
+    mainImage.onmouseover = function () {
+      this.src = "images/RIOT-NEW-Crusher-Rollover-lime_550x.png";
+    };
+    mainImage.onmouseout = function () {
+      this.src = "images/RIOT-Web-Cans-CitrusLime-HP_550x.png";
+    };
+  }
+  // Hover  efekat za 'Mango'
+  else if (anything === "images/RIOT-HP-MANO_550x.png") {
+    mainImage.onmouseover = function () {
+      this.src = "images/RIOT-NEW-Crusher-Rollover-mango_550x.png";
+    };
+    mainImage.onmouseout = function () {
+      this.src = "images/RIOT-HP-MANO_550x.png";
+    };
+  }
+  // Uklanja hover efekat za druge slike
+  else {
+    mainImage.onmouseover = null;
+    mainImage.onmouseout = null;
+  }
 }
 
 function colorChange(color) {
